1355bb0e05b86a185f35b152d590d1187323a3c2
aphd <aphidarium@proton.me>
2025-08-08 19:06:30 +0100

    gather terms via h1

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
 import { Plugin } from 'obsidian';
 
 export default class Gloss extends Plugin {
+  terms: string[] = [];
+
 	async onload() {
+    // https://publish.obsidian.md/liam/Obsidian/API+FAQ/filesystem/getMarkdownFiles+returns+an+empty+array+in+onLoad
+    this.app.workspace.onLayoutReady(() => {
+      const mdfiles = this.app.vault.getMarkdownFiles();
+      const glossary = mdfiles.find((el) => el.basename == "Glossary");
+
+      this.app.vault.read(glossary).then((result) => {
+        const arr = [...result.matchAll(/# [A-Za-z]+/g)];
+
+        for (let i = 0; i < arr.length; i++) {
+          this.terms.push(arr[i][0].slice(2));
+        }
+      })
+
+      // console.log(this.terms);
+    })
 	}
 
 	onunload() {
