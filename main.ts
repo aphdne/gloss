7f8693f8ebd7cd818188a606e8c2feb9ff1b1a1e
aphd <aphidarium@proton.me>
2025-08-11 00:08:30 +0100

    direct to separate glossary files; Definition struct

--- main.ts
+++ main.ts
@@ -3,13 +3,19 @@ import { MarkdownView, Plugin } from 'obsidian';
 /*
  * TODO: automatic glossary term insertion (without a command)
  * TODO: support multiple glossary files
+ * - needs to maintain case in original word
 */
 
+interface Definition {
+  term: string;
+  glossary: string;
+}
+
 export default class Gloss extends Plugin {
-  terms: string[] = [];
+  definitions: Definition[] = [];
 
 	async onload() {
-    // https://publish.obsidian.md/liam/Obsidian/API+FAQ/filesystem/getMarkdownFiles+returns+an+empty+array+in+onLoad
+    // use onLayoutReady(): https://publish.obsidian.md/liam/Obsidian/API+FAQ/filesystem/getMarkdownFiles+returns+an+empty+array+in+onLoad
     // grab all glossary terms
     this.app.workspace.onLayoutReady(() => {
       const glossaries = this.app.vault.getMarkdownFiles().filter((tfile) => {
@@ -25,10 +31,14 @@ export default class Gloss extends Plugin {
           const arr = [...result.matchAll(/(?<=\# )[A-Za-z]+/g)];
 
           for (let i = 0; i < arr.length; i++) {
-            this.terms.push(arr[i][0].toLowerCase()); // slice to remove '# ' prefix
+            this.definitions.push({
+              term: arr[i][0].toLowerCase(),
+              glossary: g.basename
+            });
           }
         })
       }
+      console.log(this.definitions);
     });
 
     this.addCommand({
@@ -50,12 +60,14 @@ export default class Gloss extends Plugin {
     const renderer = this.app.workspace.getActiveViewOfType(MarkdownView).previewMode.renderer;
 
     let text = renderer.text;
-    for (const term of this.terms) {
+    for (const def of this.definitions) {
+      const term = def.term;
+      const gloss = def.glossary;
       // https://forum.obsidian.md/t/is-there-a-pre-render-pre-processor-callback/72530/5
       // replace plural
-      text = text.replaceAll(new RegExp(`${term + "s"}(?!\\]|\\||s)`, "g"), "[[glossary.md#" + term + "|" + term + "s]]");
+      text = text.replaceAll(new RegExp(`${term + "s"}(?!\\]|\\||s)`, "g"), "[[" + gloss + ".md#" + term + "|" + term + "s]]");
       // replace singular
-      text = text.replaceAll(new RegExp(`${term}(?!\\]|\\||s)`, "g"), "[[glossary.md#" + term + "|" + term + "]]");
+      text = text.replaceAll(new RegExp(`${term}(?!\\]|\\||s)`, "g"), "[[" + gloss + ".md#" + term + "|" + term + "]]");
     }
     renderer.set(text);
   }
