7d5b09a36978109fedad6421c18676a29d9d5b32
aphd <aphidarium@proton.me>
2025-08-10 23:46:42 +0100

    multiple glossary files

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 import { MarkdownView, Plugin } from 'obsidian';
 
+/*
+ * TODO: automatic glossary term insertion (without a command)
+ * TODO: support multiple glossary files
+*/
+
 export default class Gloss extends Plugin {
   terms: string[] = [];
 
@@ -7,19 +12,22 @@ export default class Gloss extends Plugin {
     // https://publish.obsidian.md/liam/Obsidian/API+FAQ/filesystem/getMarkdownFiles+returns+an+empty+array+in+onLoad
     // grab all glossary terms
     this.app.workspace.onLayoutReady(() => {
-      const mdfiles = this.app.vault.getMarkdownFiles();
-      const glossary = mdfiles.find((el) => el.basename == "Glossary");
+      const glossaries = this.app.vault.getMarkdownFiles().filter((tfile) => {
+        const fm = this.app.metadataCache.getFileCache(tfile).frontmatter
+        if (fm) {
+          return fm.tags.contains("glossary");
+        }
+        return false;
+      });
 
-      if (glossary) {
-        this.app.vault.read(glossary).then((result: string) => {
+      for (const g of glossaries) {
+        this.app.vault.cachedRead(g).then((result: string) => {
           const arr = [...result.matchAll(/# [A-Za-z]+/g)];
 
           for (let i = 0; i < arr.length; i++) {
             this.terms.push(arr[i][0].slice(2).toLowerCase()); // slice to remove '# ' prefix
           }
         })
-      } else {
-        new Notice('No glossary.md!');
       }
     });
 
@@ -28,7 +36,7 @@ export default class Gloss extends Plugin {
       name: "Insert Glossary Terms",
       checkCallback: (checking: boolean) => {
         const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView.getMode() == 'preview') {
+				if (markdownView && markdownView.getMode() == 'preview') {
           if (!checking) {
             this.insertTerms();
           }
