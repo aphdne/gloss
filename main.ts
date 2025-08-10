1a1c00126ce7b7dfc34cfab7205bfdceaf107c2d
aphd <aphidarium@proton.me>
2025-08-10 22:18:02 +0100

    insert glossary terms command

--- main.ts
+++ main.ts
@@ -1,26 +1,46 @@
-import { Plugin } from 'obsidian';
+import { MarkdownView, Plugin } from 'obsidian';
 
 export default class Gloss extends Plugin {
   terms: string[] = [];
 
 	async onload() {
     // https://publish.obsidian.md/liam/Obsidian/API+FAQ/filesystem/getMarkdownFiles+returns+an+empty+array+in+onLoad
+    // grab all glossary terms
     this.app.workspace.onLayoutReady(() => {
       const mdfiles = this.app.vault.getMarkdownFiles();
       const glossary = mdfiles.find((el) => el.basename == "Glossary");
 
-      this.app.vault.read(glossary).then((result) => {
+      this.app.vault.read(glossary).then((result: string) => {
         const arr = [...result.matchAll(/# [A-Za-z]+/g)];
 
         for (let i = 0; i < arr.length; i++) {
-          this.terms.push(arr[i][0].slice(2));
+          this.terms.push(arr[i][0].slice(2).toLowerCase()); // slice to remove '# ' prefix
         }
       })
-
-      // console.log(this.terms);
     })
+
+    this.addCommand({
+      id: "gloss-insert-terms",
+      name: "Insert Glossary Terms",
+      checkCallback: (checking: boolean) => {
+        const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (markdownView.getMode() == 'preview') {
+          if (!checking) {
+            this.insertTerms();
+          }
+          return true
+        }
+      }
+    });
 	}
 
-	onunload() {
-	}
+  insertTerms() {
+    const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+    const renderer = view.previewMode.renderer;
+
+    const terms = ["apple", "banana", "chicken"];
+    for (const term of terms) {
+      renderer.set(renderer.text.replaceAll(term, "[[glossary.md#" + term + "|" + term + "]]"))
+    }
+  }
 }
